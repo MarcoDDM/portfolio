fe63bd264cdc0f4fd4d3b0f62b38530c31b7d821
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-01-02 18:33:09 -0300

    Functions for toggle

---
+++ portfolio.js
@@ -0,0 +1,27 @@
+const hideElements = () => {
+  const elements = document.querySelectorAll(' .headline, .image-down, .logo-mobile, .description, .projects, .about, .form, .footer');
+  elements.forEach(element => element.style.display = 'none');
+}
+
+const showElements = () => {
+  const elements = document.querySelectorAll('.logo-mobile, .headline, .description, .projects, .about, .form, .footer');
+  elements.forEach(element => element.style.display = 'flex');
+}
+
+function showMobileMenu() {
+  hideElements();
+  const mobileMenu = document.querySelector('.mobile_menu');
+  mobileMenu.style.display = 'flex';
+  mobileMenu.style.width = '100%';
+  mobileMenu.style.height = '454px';
+}
+
+const scrollToElement = (id) => {
+  showElements();
+  document.querySelector(id).scrollIntoView();
+}
+
+showMobileMenu;
+scrollToElement('#projects');
+scrollToElement('#about');
+scrollToElement('#form');
