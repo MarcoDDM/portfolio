094f81831a2b0840f83db7f01b75ed4e16b099ef
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-01-02 21:16:59 -0300

    Delete mobile-logo hidden

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,10 @@
 const hideElements = () => {
-  const elements = document.querySelectorAll('.hamburger, .headline, .image-down, .logo-mobile, .description, .projects, .about, .form, .footer');
+  const elements = document.querySelectorAll('.hamburger, .headline, .image-down, .description, .projects, .about, .form, .footer');
   elements.forEach(element => element.style.display = 'none');
 }
 
 const showElements = () => {
-  const elements = document.querySelectorAll('.logo-mobile, .mobile_menu, .headline, .description, .projects, .about, .form, .footer');
+  const elements = document.querySelectorAll('.mobile-logo, .mobile_menu, .headline, .description, .projects, .about, .form, .footer');
   elements.forEach(element => element.style.display = 'flex');
 }
 
@@ -12,8 +12,8 @@ function showMobileMenu() {
   hideElements();
   const mobileMenu = document.querySelector('.mobile-menu');
   mobileMenu.style.display = 'flex';
-  mobileMenu.style.width = '100%';
-  mobileMenu.style.height = '454px';
+  mobileMenu.style.width = '375px';
+  mobileMenu.style.height = '200px';
 }
 
 const scrollToElement = (id) => {
