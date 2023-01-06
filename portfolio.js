40e796ee45ff35219352bc7129e558aa850e07e4
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-01-06 07:57:17 -0300

    Dynamically generated cards

--- portfolio.js
+++ portfolio.js
@@ -55,17 +55,14 @@ links.forEach((link) => {
   });
 });
 
-const showMenuButton = document.getElementById('.burger');
-showMenuButton.addEventListener('click', showMobileMenu);
 
-const closeMenuButton = document.getElementById('.mobile-x');
-closeMenuButton.addEventListener('click', closeMenu);
 
 //RENDER CARDS DYNAMICALLY
 
 const projectsCards = [
   {
     id: 1,
+    class: 'one',
     name: 'Project name goes here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     image: 'images/component_1.png',
@@ -80,6 +77,7 @@ const projectsCards = [
   },
   {
     id: 2,
+    class: 'two',
     name: 'Project name goes here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     image: 'images/component_2.png',
@@ -94,6 +92,7 @@ const projectsCards = [
   },
   {
     id: 3,
+    class: 'three',
     name: 'Project name goes here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     image: 'images/component_3.png',
@@ -108,6 +107,7 @@ const projectsCards = [
   },
   {
     id: 4,
+    class: 'four',
     name: 'Project name goes here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     image: 'images/component_4.png',
@@ -122,6 +122,7 @@ const projectsCards = [
   },
   {
     id: 5,
+    class: 'five',
     name: 'Project name goes here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     image: 'images/component_5.png',
@@ -136,9 +137,10 @@ const projectsCards = [
   },
   {
     id: 6,
+    class: 'six',
     name: 'Project name goes here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
-    image: 'images/component_5.png',
+    image: 'images/component_6.png',
     alt: 'laptop',
     keywords: [
       'HTML/CSS',
@@ -150,4 +152,45 @@ const projectsCards = [
   }
 ]
 
-//how to render projectsCard content dynamically without bootstrap on cards div??
+
+
+function generateProjectCards(projects) {
+  const projectCards = [];
+  for (const project of projects) {
+    const projectCard = `
+      <div class="class="box ${project.class}">
+        <img class="component" src="${project.image}" alt="${project.alt}">
+        <h3 class="first-title">${project.name}</h3>
+        <ul class="keywords">
+          ${project.keywords.map(keyword => `<li>${keyword}</li>`).join('')}
+        </ul>
+        <div>
+          <a class="buttonbox" href="${project.liveVersion}">Live Version</a>
+        </div>
+      </div>
+    `;
+    projectCards.push(projectCard);
+  }
+  return projectCards.join('');
+}
+
+const projectCardsHTML = generateProjectCards(projectsCards);
+document.querySelector('.cards').innerHTML = projectCardsHTML;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const showMenuButton = document.querySelectorAll('.burger');
+// showMenuButton.addEventListener('click', showMobileMenu);
+
+// const closeMenuButton = document.querySelectorAll('.mobile-x');
+// closeMenuButton.addEventListener('click', closeMenu);
