511bf5694e883c0af2962dc332df0efd9d5eecba
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-01-06 13:26:23 -0300

    Render card elements on desktop

--- portfolio.js
+++ portfolio.js
@@ -63,9 +63,12 @@ const projectsCards = [
   {
     id: 1,
     class: 'one',
+    classDesktop: 'first',
+    classImgDesktop: 'pc-left',
     name: 'Project name goes here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     image: 'images/component_1.png',
+    imageDesktop: 'images/component_1_.png',
     alt: 'Project number one',
     keywords: [
       'HTML/CSS',
@@ -78,9 +81,12 @@ const projectsCards = [
   {
     id: 2,
     class: 'two',
+    classDesktop: 'second',
+    classImgDesktop: 'pc-left-bigger',
     name: 'Project name goes here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     image: 'images/component_2.png',
+    imageDesktop: 'images/component_5.png',
     alt: 'Project number two',
     keywords: [
       'HTML/CSS',
@@ -93,9 +99,12 @@ const projectsCards = [
   {
     id: 3,
     class: 'three',
+    classDesktop: 'pc-left-bigger',
+    classImgDesktop: 'pc-right-squares',
     name: 'Project name goes here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     image: 'images/component_3.png',
+    imageDesktop: 'images/pc_img_left_2.png',
     alt: 'Project number three',
     keywords: [
       'HTML/CSS',
@@ -108,9 +117,12 @@ const projectsCards = [
   {
     id: 4,
     class: 'four',
+    classDesktop: '',
+    classImgDesktop: 'pc-left',
     name: 'Project name goes here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     image: 'images/component_4.png',
+    imageDesktop: 'images/pc_right.png',
     alt: 'Project number four',
     keywords: [
       'HTML/CSS',
@@ -123,9 +135,12 @@ const projectsCards = [
   {
     id: 5,
     class: 'five',
+    classDesktop: '',
+    classImgDesktop: 'pc-left',
     name: 'Project name goes here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     image: 'images/component_5.png',
+    imageDesktop: 'images/component_5.png',
     alt: 'Project number five',
     keywords: [
       'HTML/CSS',
@@ -138,9 +153,12 @@ const projectsCards = [
   {
     id: 6,
     class: 'six',
+    classDesktop: '',
+    classImgDesktop: 'pc-left',
     name: 'Project name goes here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     image: 'images/component_6.png',
+    imageDesktop: 'images/component_6.png',
     alt: 'Project number six',
     keywords: [
       'HTML/CSS',
@@ -160,12 +178,23 @@ function generateProjectCards(projects) {
     const projectCard = `
       <div class="box ${project.class}">
         <img class="component" src="${project.image}" alt="${project.alt}">
+        <img class="component-1" src="${project.imageDesktop}" alt="${project.alt}">
         <h3 class="first-title">${project.name}</h3>
         <ul class="keywords">
           ${project.keywords.map(keyword => `<li>${keyword}</li>`).join('')}
         </ul>
         <div>
           <button class="buttonbox" href="${project.liveVersion}">See this project 🡲</button>
+        </div>
+      </div>
+      <div class="${project.classDesktop}">
+        <img class="component-1" src="${project.imageDesktop}" alt="${project.alt}">
+        <h3 class="first-title-">${project.name}</h3>
+        <ul class="keywords-">
+          ${project.keywords.map(keyword => `<li>${keyword}</li>`).join('')}
+        </ul>
+        <div>
+          <button class="buttonbox-" href="${project.liveVersion}">See this project 🡲</button>
         </div>
       </div>
     `;
